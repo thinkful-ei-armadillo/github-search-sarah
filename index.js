0c9c9fe6dd72f667676bd9ef01d523f669d83cd6
Sarah Drew <sarahdrew93@gmail.com>
2019-01-27 13:47:56 -0600

    fixed html, added rest of functins, nearly working.

--- index.js
+++ index.js
@@ -1,29 +1,54 @@
 'use strict';
 
+//no API_KEY needed for GitHub
 const BASE_URL= "https://api.github.com/users"
 
-
-
-function renderForm(){
-    $('.user-search-form').submit(function(event){
-        event.preventDefault();
-        const user= $('.search-form-input').val();
-        fetchSearchResults(fullURL);
-    console.log(fullURL);
-    })
+function displaySearchResults(responseJson){
+// loop through responseJson to pull info, put in <ul> as <li>s    
+    const searchResults =[];
+    for(let i=0; i < responseJson.length; i++){
+        searchResults.push( `<ul id="results"> 
+        <li>Name : ${responseJson[i].name}</li>
+        <li>Description: ${responseJson[i].description}
+        <li> URL: <a href="${responseJson[i].html_url}">${responseJson[i].html_url}</a></li>
+        </ul>`)
+    }
+    //put html of the array in the div
+$('.search-results').html(searchResults);
 }
 
 function fetchSearchResults(url){
- // get URL and then use JSON to make asynch requests   
-    fetch(url)
-    .then(response => response.json() )
-    .then (responseJson => displaySearchResults(responseJson)
-    )
-   
-    .catch(error => alert(` fetchSearchResults is not working.`))
-     console.log(responseJson);
-}
+    // get URL and then use JSON to make asynch requests   
+       fetch(url)
+       .then(response => response.json() 
+       .then (responseJson => displaySearchResults(responseJson)))
+       .catch(error => alert('fetchSearchResults is not working.'));
+        // console.log(responseJson);
+   }
 
+function createSearchURL(users) {
+    //create URLs for users.
+    //split at the spaces and then join with & so it can be read as URL    
+        const queryString= users.split(' ').join('&');
+    //create the full URL by building on the BASE_URL and adding the query string and repos     
+        const fullURL= `${BASE_URL}/${queryString}/repos`;
+    //put the fullURL through fetchResults
+    fetchSearchResults(fullURL);
+    
+    console.log(fullURL);
+    }
+
+
+function renderForm(){
+    //when the submit button is pressed, get the value of the form 
+     $('.user-search-form').submit(function(event){
+         event.preventDefault();
+         const users= $('.user-search-input').val();
+         createSearchURL(users);
+
+     console.log(fullURL);
+     })
+ }
 
 function main(){
  // the function to run all functions   
